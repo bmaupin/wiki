767ffd8346f021bd88837d4fa79219f6b1f27309
bmaupin <bmaupin@users.noreply.github.com>
2018-06-16 14:06:27 -0400

    Make subpage listings collapsible
    To make content more manageable as we add more

---
+++ assets/js/subpage-listing.js
@@ -0,0 +1,28 @@
+let subpageListing = document.getElementById('subpage-listing');
+let subpageLinks = subpageListing.getElementsByTagName('a');
+
+for (let i = 0; i < subpageLinks.length; i++) {
+  let listCollapser = document.createElement('span');
+  subpageLinks[i].parentNode.insertBefore(listCollapser, subpageLinks[i]);
+
+  // Don't show the collapse icon if there are no subelements
+  if (subpageLinks[i].nextElementSibling === null) {
+    listCollapser.className = 'list-collapser';
+    continue;
+  }
+
+  listCollapser.className = 'fas fa-chevron-down list-collapser';
+
+  // Adapted from https://stackoverflow.com/a/30945775/399105
+  listCollapser.onclick = function() {
+    let ul = this.parentElement.querySelectorAll('ul')[0];
+
+    if (ul.offsetHeight > 0) {
+      ul.style.display = 'none';
+      this.className = 'fas fa-chevron-right list-collapser';
+    } else {
+      ul.style.display = 'block';
+      this.className = 'fas fa-chevron-down list-collapser';
+    }
+  }
+}
