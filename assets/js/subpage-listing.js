99d957bd3203e9eb4c9e5950ec0786d9367df77d
bmaupin <bmaupin@users.noreply.github.com>
2018-06-17 15:13:44 -0400

    Add collapse animations for subpage listing

--- assets/js/subpage-listing.js
+++ assets/js/subpage-listing.js
@@ -13,15 +13,21 @@ for (let i = 0; i < subpageLinks.length; i++) {
 
   listCollapser.className = 'fas fa-chevron-down list-collapser';
 
+  // Set initial height manually so collapse animation will work
+  let ul = listCollapser.parentElement.querySelectorAll('ul')[0];
+  ul.style.height = ul.scrollHeight + "px";
+
   // Adapted from https://stackoverflow.com/a/30945775/399105
   listCollapser.onclick = function() {
     let ul = this.parentElement.querySelectorAll('ul')[0];
 
-    if (ul.offsetHeight > 0) {
-      ul.style.display = 'none';
+    if (ul.className !== 'collapsed') {
+      ul.className = 'collapsed';
+      ul.style.height = 0;
       this.className = 'fas fa-chevron-right list-collapser';
     } else {
-      ul.style.display = 'block';
+      ul.className = 'expanded';
+      ul.style.height = ul.scrollHeight + "px";
       this.className = 'fas fa-chevron-down list-collapser';
     }
   }
