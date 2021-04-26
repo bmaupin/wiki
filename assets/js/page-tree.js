a2a875ba8b14487be74088ccb0e5805d0995aa86
bmaupin <bmaupin@users.noreply.github.com>
2021-04-26 14:17:31 -0400

    Commit changes made by prettier

--- assets/js/page-tree.js
+++ assets/js/page-tree.js
@@ -4,7 +4,10 @@ export function configurePageListing(id) {
 
   for (let i = 0; i < pageListingLinks.length; i++) {
     let listCollapser = document.createElement('span');
-    pageListingLinks[i].parentNode.insertBefore(listCollapser, pageListingLinks[i]);
+    pageListingLinks[i].parentNode.insertBefore(
+      listCollapser,
+      pageListingLinks[i]
+    );
 
     // Don't show the collapse icon if there are no subelements
     if (pageListingLinks[i].nextElementSibling === null) {
@@ -19,7 +22,7 @@ export function configurePageListing(id) {
     }
 
     // Adapted from https://stackoverflow.com/a/30945775/399105
-    listCollapser.onclick = function() {
+    listCollapser.onclick = function () {
       let ul = this.parentElement.querySelectorAll('ul')[0];
 
       if (ul.offsetHeight > 0) {
@@ -29,6 +32,6 @@ export function configurePageListing(id) {
         ul.style.display = 'block';
         this.className = 'fas fa-chevron-down list-collapser expanded';
       }
-    }
+    };
   }
 }
