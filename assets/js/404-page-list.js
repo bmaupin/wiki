6f368ac65253a4d9a7f74cb1764f7b0d2d820304
bmaupin <bmaupin@users.noreply.github.com>
2022-05-26 13:47:53 -0400

    feat: Auto redirect on 404 page if only 1 match
    In order to facilitate moving of pages (which is something I tend to do with some frequency), the 404 page tries to find potential matches and then lists them. If only one match is found, go ahead and automatically redirect the page after a number of seconds (currently 5) to make it more user-friendly and reduce any hesitancy I may have about moving pages 😄
    
    Closes #27

--- assets/js/404-page-list.js
+++ assets/js/404-page-list.js
@@ -28,11 +28,17 @@ fetch('/wiki/assets/js/pages.json')
     let html = '';
 
     if (matchingPages.length !== 0) {
-      html = `
-        <p>The page you're looking for may have been moved. Here are some possible matches:</p>
-        <ul>
-      `;
+      if (matchingPages.length === 1) {
+        html = `<p>The page you're looking for may have been moved. Only one match was found, so you will be redirected in a few seconds:</p>`;
 
+        setTimeout(function () {
+          window.location.href = matchingPages[0].url;
+        }, 5000);
+      } else {
+        html = `<p>The page you're looking for may have been moved. Here are some possible matches:</p>`;
+      }
+
+      html += '<ul>';
       for (const page of matchingPages) {
         // Add the URL below the title for easier differentiation in the case of multiple matches
         html += `
