1a48942a10bc4a74d65282e0b9fa0b3448896045
bmaupin <bmaupin@users.noreply.github.com>
2026-01-14 14:16:24 -0500

    feat: Serve files at /wiki
    This is because the site will be deployed at https://bmaupin.github.io/wiki/

--- app/routes.ts
+++ app/routes.ts
@@ -2,6 +2,6 @@ import { index, route, type RouteConfig } from '@react-router/dev/routes';
 
 export default [
   index('routes/home.tsx'),
-  route('docs/*', 'docs/page.tsx'),
+  route('wiki/*', 'docs/page.tsx'),
   route('api/search', 'docs/search.ts'),
 ] satisfies RouteConfig;
