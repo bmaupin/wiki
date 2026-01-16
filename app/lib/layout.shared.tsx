3b801dc8c52f5611e2d145e6db6b4e2d7abaffb9
bmaupin <bmaupin@users.noreply.github.com>
2026-01-16 13:40:56 -0500

    feat: Rename top left link
    ... and link to /wiki (default is to link to /)

--- app/lib/layout.shared.tsx
+++ app/lib/layout.shared.tsx
@@ -3,7 +3,8 @@ import type { BaseLayoutProps } from 'fumadocs-ui/layouts/shared';
 export function baseOptions(): BaseLayoutProps {
   return {
     nav: {
-      title: 'React Router',
+      title: 'My wiki',
+      url: '/wiki',
     },
   };
 }
