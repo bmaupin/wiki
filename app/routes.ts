1e1fabe0fd5e8fe67eacc7d7eb235ad50b11440b
bmaupin <bmaupin@users.noreply.github.com>
2026-01-16 13:56:41 -0500

    feat: Replace home page with redirect to /wiki

--- app/routes.ts
+++ app/routes.ts
@@ -1,7 +1,7 @@
 import { index, route, type RouteConfig } from '@react-router/dev/routes';
 
 export default [
-  index('routes/home.tsx'),
+  index('routes/redirect.tsx'),
   route('wiki/*', 'docs/page.tsx'),
   route('api/search', 'docs/search.ts'),
 ] satisfies RouteConfig;
