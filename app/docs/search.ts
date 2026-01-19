1bb62ee97468016b15c1d1f6110b5a7b1ec526c4
bmaupin <bmaupin@users.noreply.github.com>
2026-01-19 13:02:30 -0500

    feat: Only index page titles
    There are over 250 pages and this probably won't go down, so this speeds up search.
    
    Some alternative approaches:
    
    - index titles and headings but not content
    - index titles and headings but don't index pages under archive

--- app/docs/search.ts
+++ app/docs/search.ts
@@ -1,8 +1,26 @@
+// See here for docs for this file: https://www.fumadocs.dev/docs/headless/search/orama
+
 import { createFromSource } from 'fumadocs-core/search/server';
 import { source } from '@/lib/source';
 
 const server = createFromSource(source, {
   language: 'english',
+  buildIndex(page) {
+    return {
+      title: page.data.title,
+      description: page.data.description,
+      url: page.url,
+      id: page.url,
+      // Use this instead to index everything (or better yet replace createSearchAPI with createFromSource)
+      // structuredData: page.data.structuredData,
+      structuredData: {
+        // Use this instead to index page headings
+        // headings: page.data.structuredData.headings,
+        headings: [],
+        contents: [],
+      },
+    };
+  },
 });
 
 export async function loader() {
