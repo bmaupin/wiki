3bde1ad866dc6ef8b09482dcb7e724c0a73ab508
bmaupin <bmaupin@users.noreply.github.com>
2026-01-21 12:59:20 -0500

    fix: Only serve root redirect route locally

--- app/routes.ts
+++ app/routes.ts
@@ -1,7 +1,14 @@
 import { index, route, type RouteConfig } from '@react-router/dev/routes';
 
-export default [
-  index('routes/redirect.tsx'),
+const routes: RouteConfig = [
   route('wiki/*', 'docs/page.tsx'),
-  route('api/search', 'docs/search.ts'),
-] satisfies RouteConfig;
+  route('wiki/api/search', 'docs/search.ts'),
+];
+
+// Only serve these routes when doing local development
+if (import.meta.env.DEV) {
+  // Does a server-side redirect
+  routes.unshift(index('routes/redirect.tsx'));
+}
+
+export default routes;
