1e20bbf3d084a9b4bc1d4030d267f3197d4df47c
bmaupin <bmaupin@users.noreply.github.com>
2026-01-23 13:54:11 -0500

    feat: Serve at /wiki

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -18,7 +18,7 @@ const config: Config = {
   url: 'https://bmaupin.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/wiki/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
@@ -87,7 +87,7 @@ const config: Config = {
           label: 'Docs',
         },
         {
-          href: 'https://github.com/facebook/docusaurus',
+          href: 'https://github.com/bmaupin/wiki/',
           label: 'GitHub',
           position: 'right',
         },
