4be9036c4290433186eecd950fd186b58dce6369
bmaupin <bmaupin@users.noreply.github.com>
2026-01-29 13:10:15 -0500

    feat: Add notice to all archived pages

--- src/theme/MDXContent/index.tsx
+++ src/theme/MDXContent/index.tsx
@@ -1,13 +1,24 @@
-import React, {type ReactNode} from 'react';
+import React, { type ReactNode } from 'react';
 import MDXContent from '@theme-original/MDXContent';
 import type MDXContentType from '@theme/MDXContent';
-import type {WrapperProps} from '@docusaurus/types';
+import type { WrapperProps } from '@docusaurus/types';
+import Admonition from '@theme/Admonition';
+import { useLocation } from '@docusaurus/router';
 
 type Props = WrapperProps<typeof MDXContentType>;
 
 export default function MDXContentWrapper(props: Props): ReactNode {
+  const location = useLocation();
+
   return (
     <>
+      {location.pathname.startsWith('/wiki/archive') && (
+        <Admonition title="Archived" type="warning">
+          <p>
+            This page has been archived and will receive no further updates.
+          </p>
+        </Admonition>
+      )}
       <MDXContent {...props} />
     </>
   );
